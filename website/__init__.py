b4e5bc83de4ea5932b1b523072f2594a9e0a09c6
chloezhang714 <47944883+chloezhang714@users.noreply.github.com>
2023-01-16 00:27:06 -0500

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -4,11 +4,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from dotenv import load_dotenv
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
+    migrate = Migrate(app, db)
     app = Flask(__name__, static_folder='static')
     app.config['SECRET_KEY'] = 'secret'
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
