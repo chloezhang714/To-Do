159dd0f311cf6d3f7ff3d245b95c606e704862f9
chloezhang714 <47944883+chloezhang714@users.noreply.github.com>
2023-01-16 00:45:33 -0500

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -9,6 +9,7 @@ db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
+    app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret'
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     load_dotenv()
