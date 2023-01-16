981665163a6417e393e84d628d83db30723a3ede
chloezhang714 <47944883+chloezhang714@users.noreply.github.com>
2023-01-16 00:43:56 -0500

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -4,7 +4,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from dotenv import load_dotenv
-from flask_migrate import Migrate
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
